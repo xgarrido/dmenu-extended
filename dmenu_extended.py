2139fb62199817bbb422823c1bebc44ea50582e8
Xavier Garrido <xavier.garrido@gmail.com>
2015-12-05 11:41:44 +0100

    use utf8 arrow

--- dmenu_extended.py
+++ dmenu_extended.py
@@ -97,7 +97,7 @@ default_prefs = {
     "filebrowser": "xdg-open",          # Program to handle opening paths
     "webbrowser": "xdg-open",           # Program to hangle opening urls
     "terminal": "xterm",                # Terminal
-    "indicator_submenu": "->",          # Symbol to indicate a submenu item
+    "indicator_submenu": "→",           # Symbol to indicate a submenu item
     "indicator_edit": "*",              # Symbol to indicate an item will launch an editor
     "indicator_alias": "#"              # Symbol to indecate an aliased command
 }
